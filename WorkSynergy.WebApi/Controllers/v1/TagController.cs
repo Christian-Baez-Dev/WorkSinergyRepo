957ef85c13e00126a2267f4a7ad3a3b28af129e9
OBaezInfo <oscarbaez.info@gmail.com>
2024-11-10 17:10:57 -0400

    Creating middlewares and BaseApiController

---
+++ WorkSynergy.WebApi/Controllers/v1/TagController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace WorkSynergy.WebApi.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    //[Authorize]
+    [SwaggerTag("Controller for the Tags")]
+    public class TagController : BaseApiController
+    {
+
+    }
+}
